1c837057516cb1ec572eb3d5d55332ff4291a966
Huerte <huertejerald@gmail.com>
2025-05-14 14:22:21 +0800

    Added a migration module in cart directory

--- gadget_store/orders/forms.py
+++ gadget_store/orders/forms.py
@@ -6,7 +6,7 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address', 'city', 'state', 'zip_code']
+        fields = ['id', 'first_name', 'last_name', 'email', 'address', 'city', 'state', 'zip_code']
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
